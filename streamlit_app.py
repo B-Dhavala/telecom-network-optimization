91210faeebae0e70cecd4726f8584a8d072b52c6
B-Dhavala <dhavala2310@gmail.com>
2024-11-18 11:39:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.sidebar:
         'Timestamp': st.text_input('Timestamp (MM/DD/YYYY HH MM)', '5/9/2023 9:29'),
         'Caller_ID': st.number_input('Caller ID', min_value=1, max_value=10000, step=1),
         'Call_Duration_Seconds': st.number_input('Call Duration (Seconds)', min_value=0, step=1),
-        'Data_Transferred_MB': st.number_input('Data Transferred (MB)', min_value=0.0, step=0.1),
+        'Data_Transferred': st.number_input('Data Transferred', min_value=0.0, step=0.1),
         'Signal_Strength': st.number_input('Signal Strength (dBm)', min_value=-100.0, step=0.1),
         'Time_to_Connect_Seconds': st.number_input('Time to Connect (Seconds)', min_value=0.0, step=0.1),
         'Caller_Location': st.selectbox('Caller Location', ['Surat', 'Mumbai', 'Delhi', 'Chennai', 'Jaipur', 'Nagpur', 'Kolkata', 'Ahmedabad', 'Bengaluru', 'Hyderabad', 'Lucknow', 'Pune']),
@@ -26,7 +26,6 @@ with st.sidebar:
         'Service_Type': st.selectbox('Service Type', ['Voice', 'Video', 'Data']),
         'Network_Tower_ID': st.selectbox('Network Tower ID', ['Tower_1', 'Tower_2', 'Tower_3', 'Tower_4', 'Tower_5', 'Tower_6', 'Tower_7', 'Tower_8', 'Tower_9', 'Tower_10', 'Tower_11', 'Tower_12', 'Tower_13', 'Tower_14', 'Tower_15', 'Tower_16', 'Tower_17', 'Tower_18', 'Tower_19']),
         'Customer_Plan': st.selectbox('Customer Plan', ['Prepaid', 'Postpaid']),
-        'Call_Direction': st.selectbox('Call Direction', ['Incoming', 'Outgoing']),
         'Roaming_Status': st.selectbox('Roaming Status', ['Yes', 'No']),
         'Network_Congestion_Level': st.selectbox('Network Congestion Level', ['Low', 'Medium', 'High'])
     }
@@ -59,11 +58,11 @@ predicted_bandwidth = xg_model_.predict(sample_df)
 with st.expander('Input Data'):
     st.write(sample_df)
 
-st.subheader('Predicted Allocated Bandwidth (MB):')
+st.subheader('Predicted Allocated Bandwidth:')
 
 predicted_value_html = f"""
     <p style="font-size: 20px; color: blue; font-weight: bold;">
-        {predicted_bandwidth[0]:.2f} MB
+        {predicted_bandwidth[0]:.2f}
     </p>
 """
 st.markdown(predicted_value_html, unsafe_allow_html=True)
